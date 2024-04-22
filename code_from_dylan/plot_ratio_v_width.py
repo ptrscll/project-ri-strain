9e741082eaa830f701b89693b128c0d4db9647f6
ptrscll <ptrscll@gmail.com>
2024-04-22 12:49:38 -0400

    improved formatting

--- code_from_dylan/plot_ratio_v_width.py
+++ code_from_dylan/plot_ratio_v_width.py
@@ -4,10 +4,46 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 
 
 side_path = r'predef_results/100km/strain_sides_time/noninitial_plots/side_data.txt'
 width_path = r'predef_results/100km/strain_sides_time/noninitial_plots/diff_data_max_10_cutoff.txt'
+
+# Code to add arrows to plots
+# Adapted from https://stackoverflow.com/questions/34017866/arrow-on-a-line-plot
+def add_arrow(line, position=None, direction='right', size=15, color=None, start_ind = None):
+    """
+    add an arrow to a line.
+
+    line:       Line2D object
+    position:   x-position of the arrow. If None, mean of xdata is taken
+    direction:  'left' or 'right'
+    size:       size of the arrow in fontsize points
+    color:      if None, line color is taken.
+    """
+    if color is None:
+        color = line.get_color()
+
+    xdata = line.get_xdata()
+    ydata = line.get_ydata()
+
+    if position is None:
+        position = xdata.mean()
+    # find closest index
+    if start_ind == None:
+        start_ind = np.argmin(np.absolute(xdata - position))
+    if direction == 'right':
+        end_ind = start_ind + 1
+    else:
+        end_ind = start_ind - 1
+
+    line.axes.annotate('',
+        xytext=(xdata[start_ind], ydata[start_ind]),
+        xy=(xdata[end_ind], ydata[end_ind]),
+        arrowprops=dict(arrowstyle="->", color=color),
+        size=size
+    )
 
 
 # Getting side ratios
@@ -51,12 +87,17 @@ data_file.close()
 
 # Plotting data
 for i in range(0, 8):
-    ax.plot(R_L[i], widths[i], label='Model ' + str(i), marker='X', markevery=[-1])
+    line = ax.plot(R_L[i], widths[i], label='Model ' + str(i + 1), marker='X', markevery=[-1])[0]
+    add_arrow(line, start_ind=1)
+    add_arrow(line, start_ind=4)
+    add_arrow(line, start_ind=7)
 
 
 # Making nicer formatting
 ax.set_xlabel('Right Side Strain : Left Side Strain')
-ax.set_ylabel('Suture Width')
+ax.set_ylabel('Suture Width (km)')
+plt.tick_params(axis='x', which='minor')
+ax.xaxis.set_minor_formatter(FormatStrFormatter("%.1f"))
 
 ax.legend()
 ax.set_xscale('log')
